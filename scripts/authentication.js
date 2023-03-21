195b07a16d60738d0ac7f0a279d7778713ee754e
Vitor Guara <vrosaguara@my.bcit.ca>
2023-03-21 10:05:32 -0700

    demo 11 done

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -20,7 +20,7 @@ var uiConfig = {
           db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
                  name: user.displayName,                    //"users" collection
                  email: user.email,                         //with authenticated user's ID (user.uid)
-                 country: "Canada",                      //optional default profile info      
+                 city: "Vancouver",                      //optional default profile info      
                  school: "BCIT"                          //optional default profile info
           }).then(function () {
                  console.log("New user added to firestore");
